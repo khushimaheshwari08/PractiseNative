31cbfa98e8101f888605e71202f6d5901147d4a2
khushimaheshwari08 <khushimaheshwari869@gmail.com>
2022-11-21 19:04:34 +0530

    loading

---
+++ App.js
@@ -0,0 +1,57 @@
+import React, { useEffect, useState } from 'react';
+import Login from './src/Components/Login/Login';
+
+import Dashboard from './src/Components/Dashboard/Dashboard';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import SignUp from './src/Components/Signup/SignUp';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+
+  const [auth, setAuth] = useState(null)
+  const [render, setRender] = useState(false)
+
+  const loginData = async() => {
+      let retrivedItem = await AsyncStorage.getItem('isLogin');
+      let item = JSON.parse(retrivedItem);
+      setAuth(item);
+      setRender(true)
+  }
+
+  useEffect(() => {
+    loginData();
+  }, [])
+  
+  return (
+    <>
+    {render ?(
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName={auth===true ?"dashboard":"login"}>
+          <Stack.Screen
+            options={{headerShown: false}}
+            name="login"
+            component={Login}
+          />
+          <Stack.Screen
+            options={{headerShown: false}}
+            name="signUp"
+            component={SignUp}
+          />
+          <Stack.Screen
+            options={{headerShown: false}}
+            name="dashboard"
+            component={Dashboard}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    ): null
+      }
+    </>
+  );
+};
+
+export default App;
---
+++ src/Components/Dashboard/Dashboard.js
@@ -0,0 +1,53 @@
+import { useNavigation } from '@react-navigation/native';
+import React from 'react';
+import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const Dashboard = () => {
+
+  const navigation = useNavigation();
+
+  const onLogout = async() => {
+      await AsyncStorage.setItem('isLogin', JSON.stringify(false));
+      navigation.replace('login');
+    } 
+
+  return (
+
+    <View style={styles.container}>
+           <TouchableOpacity onPress={() => onLogout()}>
+          <View style={styles.logoutBtn}>
+            <Text style={styles.logoutText}>Logout</Text>
+          </View>
+          </TouchableOpacity>
+          </View>
+        
+   
+  );
+};
+
+export default Dashboard;
+
+const styles = StyleSheet.create({
+
+  container: {
+    backgroundColor: '#fff',
+    flex: 1,
+  },
+  
+  logoutBtn: {
+    marginTop:30,
+    marginLeft: 15,
+    marginRight: 15,
+    height: 50,
+    width:80,
+    backgroundColor: '#da1b3f',
+    borderRadius: 6,
+  },
+
+  logoutText: {
+    paddingTop: 15,
+    textAlign: 'center',
+    color: 'white',
+  }
+})
